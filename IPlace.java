fb55ee8e2698c298836f3333004382325a486674
Sangho Jeon <sjeon36@wisc.edu>
2022-04-14 02:41:10 +0000

    added IPlace.java

---
+++ IPlace.java
@@ -0,0 +1,20 @@
+/**
+ * Instances of classes that implement this interface represents a place
+ * that can be stored, sorted, and used with the accessors below.
+ */
+public interface IPlace extends Comparable<IPlace> {
+    /**
+     * constructor args
+     * Place(String name, double latitude, double longitude)
+     * name - name of the place
+     * latitude - latitude of the place geographically
+     * longitude - longitude of the place geographically
+     */
+
+    String getName(); // return the name of the place
+
+    double getLatitude(); // return the latitude of the place
+
+    double getLongitude(); // return the longitude of the place
+
+}
---
+++ IPlaceLoader.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.io.FileNotFoundException;
+
+/**
+ * Instances of classes that implement this interface can be used to load a list of places from a
+ * specified tsv source file.
+ * The following parts are used to load these show attributes:
+ *        name: what the place is called
+ *        latitude: geographical latitude of the place
+ *        longitude: geographical longitude of the place
+ */
+public interface IPlaceLoader {
+
+    /**
+     * This method returns the array of places described in the tsv file
+     * @param filePath the file path of the tsv file
+     * @return the array of places described in the tsv file
+     * @throws FileNotFoundException if the filepath is not a tsv file or cannot be read
+     */
+    IPlace loadPlaces(String filePath) throws FileNotFoundException;
+
+}
