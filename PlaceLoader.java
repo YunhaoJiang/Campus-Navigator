d62eb87f275ce64324c565a49d44c38ea2e4fb29
Sangho Jeon <sjeon36@wisc.edu>
2022-04-28 23:19:13 +0000

    added PlaceLoader.java

---
+++ PlaceLoader.java
@@ -0,0 +1,66 @@
+import java.io.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class PlaceLoader implements IPlaceLoader {
+    protected List<IPlace> location;
+    
+    public int imageSizeX = 2082;
+    public int imageSizeY = 1482;
+
+    public PlaceLoader()
+    {
+        this.location = new ArrayList<>();
+    }
+   
+
+    /**
+     * This method returns the list of words described in the xml file
+     *
+     * @param filePath the file path of the xml file
+     * @return the list of words described in the xml file
+     * @throws FileNotFoundException if the filepath is not a xml file or cannot be read
+     */
+    @Override
+    public List<IPlace> loadPlaces(String filePath) throws FileNotFoundException {
+    	JSONParser parser = new JSONParser();
+   	 
+    	try {
+     
+    		Object obj = parser.parse(new FileReader(filePath));
+     
+    		JSONObject jsonObject = (JSONObject) obj;
+     
+    		
+    		
+    		//Object
+    		JSONArray array = (JSONArray) jsonObject.get("places");
+    		for(int i=0; i<array.size(); i++){
+    			JSONObject result = (JSONObject) array.get(i);
+
+    			
+    			Place p = new Place((String)result.get("name"), (int)((Double)result.get("x")*imageSizeX), (int)((Double)result.get("y")*imageSizeY));
+    			location.add(p);
+    		}
+    		
+    		
+    		
+    	} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	} catch (ParseException e) {
+    		e.printStackTrace();
+    	}
+        
+
+        return location;
+    }
+}
