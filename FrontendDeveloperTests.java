c4ff3d42a6cd9746632d586dc4867a87434494ca
Danny Jiang <cjiang88@wisc.edu>
2022-05-04 04:30:11 +0000

    added two tests inside FrontendDeveloperTests

--- FrontendDeveloperTests.java
+++ FrontendDeveloperTests.java
@@ -6,6 +6,9 @@
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.NoSuchElementException;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
@@ -127,6 +130,56 @@ public class FrontendDeveloperTests extends JavaFXTester {
 		assertEquals("Select location for: \"Unnamed\"", enteredName.getText());
 	}
 	
+	/**
+	 * Checks if the correct path from Memorial Union to Kohl Center is displayed correctly
+	 */
+	@Test
+	public void test6() {
+		// lookup buttons to click
+		Button memU= lookup("#MemorialUnion").query();
+		Button kohl = lookup("#KohlCenter").query();
+		Button navigate = lookup("#navigate").query();
+
+		interact(() -> {
+			// Clicks navigate
+			navigate.fireEvent(new ActionEvent());
+			// Clicks Memorial Union
+			memU.fireEvent(new ActionEvent());
+			// Clicks Kohl Center
+			kohl.fireEvent(new ActionEvent());
+			Button submit = lookup("#submitButton").query();
+			// Clicks submitButton
+			submit.fireEvent(new ActionEvent());
+		});
+		Label path = lookup("#path").query();
+		// Checks the path printed out
+		assertEquals("Memorial Union -> Chadbourne -> Kohl Center", path.getText());
+	}
 	
+	/**
+	 * Checks if the an exception is thrown when two yellow nodes are chosen for navigation
+	 */
+	@Test
+	public void test7() {
+		// lookup buttons to click
+		Button randall = lookup("#CampRandall").query();
+		Button uSouth = lookup("#UnionSouth").query();
+		Button navigate = lookup("#navigate").query();
+		assertThrows(Exception.class, () -> {
+			interact(() -> {
+			// Clicks navigate
+			navigate.fireEvent(new ActionEvent());
+			// Clicks Memorial Union
+			randall.fireEvent(new ActionEvent());
+			// Clicks Kohl Center
+			uSouth.fireEvent(new ActionEvent());
+			Button submit = lookup("#submitButton").query();
+			// Clicks submitButton
+			submit.fireEvent(new ActionEvent());
+		});
+		});
+		
+	}
+
 
 }
