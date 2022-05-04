0eac76b447c8a9453a32d2ae6a7be086077f268f
Danny Jiang <cjiang88@wisc.edu>
2022-05-04 05:12:04 +0000

    Added two extra tests for another role in FrontendDeveloperTests.java

--- FrontendDeveloperTests.java
+++ FrontendDeveloperTests.java
@@ -180,6 +180,34 @@ public class FrontendDeveloperTests extends JavaFXTester {
 		});
 		
 	}
+	@Test
+	public void otherTest1() {
+		Map map = new Map();
+		// Creates 3 hypothetical location to test out the weight calculation function
+	    IPlace loc1 = new Place("loc1",0,0);
+	    IPlace loc2 = new Place("loc2",0,15);
+	    IPlace loc3 = new Place("loc3",3,19);
+	    // Insert the locations into the map and connect them
+	    // Assigning weights with the function
+	    map.insertVertex(loc1);
+	    map.insertVertex(loc2);
+	    map.insertVertex(loc3);
+	    map.insertEdge(loc1,loc2,map.calculateWeight(loc1,loc2));
+	    map.insertEdge(loc2,loc3,map.calculateWeight(loc2,loc3));
+	    // The total weight should be 15 + 5 = 20
+	    assertEquals(20,map.calculateTime(loc1,loc3,1));
+	}
+	@Test
+	public void otherTest2() {
+		Map map = new Map();
+		// Creates two locations that are 50 pixels apart
+	    IPlace loc1 = new Place("loc1",0,0);
+	    IPlace loc2 = new Place("loc2",30,40);
+	    // The weight of the edge between these two locations should be 50
+	    int weight = map.calculateWeight(loc1, loc2);
+	    assertEquals(50, weight);
+	}
+	
 
 
 }
